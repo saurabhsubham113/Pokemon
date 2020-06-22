b5e59f04ba334ee68d9c92324ac6cf3c82023950
subham saurabh <saurabhsubham113@gmail.com>
2020-06-22 20:36:00 +0530

    Updates

---
+++ precache-manifest.aaaa4599d64376f7a006db416b6d8728.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05654b09170df0a89842cafe7f03085",
+    "url": "/Pokemon/index.html"
+  },
+  {
+    "revision": "8edc7f89d78ffd8e6da3",
+    "url": "/Pokemon/static/css/main.61ac26e4.chunk.css"
+  },
+  {
+    "revision": "c0c20a8e4ef3773acb5b",
+    "url": "/Pokemon/static/js/2.1cb0c0e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon/static/js/2.1cb0c0e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edc7f89d78ffd8e6da3",
+    "url": "/Pokemon/static/js/main.c49f6d06.chunk.js"
+  },
+  {
+    "revision": "5b4cd58d61972634c9a1",
+    "url": "/Pokemon/static/js/runtime-main.221964bd.js"
+  }
+]);
